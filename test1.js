7d07593bc0fbf6a34412aa2e2630e852c867ee8a
tanmaydesai07 <tanmay2004desai@gmail.com>
2024-12-07 11:45:40 +0530

    added variables

--- test1.js
+++ test1.js
@@ -1,9 +1,18 @@
 const accountId=1212;
 let accountEmail="8B2Gg@example.com";
-let accountPassword="12345";
+let accountPassword="12345he";
 accountCity="Jaipur";
 
 console.log(accountId);
 
 console.table([accountId,accountEmail,accountPassword,accountCity]);
-console.log(typeof accountCity);
+// console.log(typeof accountCity);
+accountPassword= String(accountPassword);
+console.log(typeof(accountPassword));
+console.log(accountPassword);
+let test1=5*2;
+console.log(+test1); 
+
+let num1,num2, num3;
+num1=num2=num3=2+2;
+console.log(num2);
