0dc2d9ed1ea8d97951cac0375fb820fb18c3db96
tanmaydesai07 <tanmay2004desai@gmail.com>
2024-12-07 10:58:59 +0530

    datatypes

---
+++ test1.js
@@ -0,0 +1,9 @@
+const accountId=1212;
+let accountEmail="8B2Gg@example.com";
+let accountPassword="12345";
+accountCity="Jaipur";
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity]);
+console.log(typeof accountCity);
